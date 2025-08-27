ba65eeba726253a9dbddb0f5b49209b87e578fd7
Gideon <tranminhquan.dtp1b@gmail.com>
2025-08-27 11:49:53 +0700

    fix(ui-login): fix position right contents

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    projectId: "rzomie-4d56b",
+    appId: "1:1066506289495:web:b472905faf07e84b72be12",
+    storageBucket: "rzomie-4d56b.firebasestorage.app",
+    apiKey: "AIzaSyB19jeacZpC8hVo89cATGgEwQJ7ZWxbhKY",
+    authDomain: "rzomie-4d56b.firebaseapp.com",
+    messagingSenderId: "1066506289495"
+  },
+  baseUrl: "http://localhost:3000"
+};
